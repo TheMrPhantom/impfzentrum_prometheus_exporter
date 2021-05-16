c367f60300b6fa4ff6d2d499b4f4b825629e5266
hawkeye <sqrt@hawkeye>
2021-05-16 22:51:09 +0200

    working

--- checker.py
+++ checker.py
@@ -1,6 +1,11 @@
+from datetime import date, datetime
+from os import lseek
+from warnings import catch_warnings
 from selenium import webdriver
+
 from selenium.webdriver.firefox.firefox_profile import FirefoxProfile
 from selenium.webdriver.firefox.options import Options
+
 from selenium.webdriver import ActionChains
 from selenium.webdriver import DesiredCapabilities
 import json
@@ -10,34 +15,64 @@ import sys
 import time
 from envir import proxy_port, proxy_hops
 import tor
-
+from termcolor import colored
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Checker:
 
-    def __init__(self):
+    def __init__(self, ip_function=None):
         print("Init browser")
-
+        self.ip_function = ip_function
         self.prox = tor.Proxy_Handler()
+        print("Proxy initial start")
         self.prox.start_proxy()
+        print("Proxy initial start done")
 
         profile = FirefoxProfile()
         profile.set_preference("dom.webdriver.enabled", False)
-        profile.set_preference('useAutomationExtension', False)
+        profile.set_preference('useAutomationExtension', False)  
+        profile.set_preference("devtools.jsonview.enabled", False)  
         profile.set_preference("general.useragent.override",
                                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
         profile.update_preferences()
 
         profile.set_preference("network.proxy.type", 1)
-        profile.set_preference("network.proxy.http", "localhost")
-        profile.set_preference("network.proxy.http_port", int(proxy_port))
+        profile.set_preference("network.proxy.socks", "localhost")
+        profile.set_preference("network.proxy.socks_port", int(proxy_port))
 
         desired = DesiredCapabilities.FIREFOX
         options = Options()
         options.headless = True
+        options.add_argument("-devtools")
         self.driver = webdriver.Firefox(
             options=options, firefox_profile=profile, desired_capabilities=desired)
         signal.signal(signal.SIGINT, self.kill_signal_handler)
         print("Finished...")
+
+    def make_stable_tor_connection(self, output=False):
+        connection_stable = False
+        ip_found = None
+        while not connection_stable:
+            try:
+                self.prox.restart_proxy(output)
+                self.driver.get("http://api.ipify.org?format=json")
+                self.driver.save_screenshot("connection.png")
+                div_xpath = "//pre"
+                content_element = self.driver.find_element_by_xpath(div_xpath)
+                page_content = content_element.get_attribute("innerHTML")
+                if ip_found != page_content:
+                    print(colored(datetime.now(), "yellow"))
+                ip_found = json.loads(page_content)
+                print("\t", colored(page_content, "yellow"))
+                connection_stable = True
+                
+            except:
+                time.sleep(0.5)
+                traceback.print_exc()
+                print(colored("No stable tor connection", "red"))
+        return ip_found['ip']
 
     def kill_signal_handler(self, sig, frame):
         print('SIGINT incoming')
@@ -46,65 +81,75 @@ class Checker:
         sys.exit(0)
 
     def getVacStatus(self, center):
-        self.driver.get("http://httpbin.org/ip")
-        print(self.driver.page_source)
+
         url = center["URL"]
         plz = center["PLZ"]
+
+        ip = self.make_stable_tor_connection()
+        self.ip_function(ip, center)
+        print("Load base page")
+        try:
+            self.driver.get(url)
+            wait = WebDriverWait(self.driver, 10)
+            men_menu = wait.until(EC.visibility_of_element_located((By.XPATH, "(//body//div[@class='app-wrapper']//div[contains(@class,'d-flex')]/div[@class='page-ets']/div[@class='container']//div[contains(@class,'row')]/div[contains(@class,'offset-1')]/form[contains(@class,'ng-untouched')]/div[contains(@class,'form-group')])[4]")))
+            ActionChains(self.driver).move_to_element(men_menu).click().perform()
+        except:
+            pass
+        print("Done")
+
+
         output = None
         special = None
-        for trys in range(0, 2):
-            in_waitingroom, waitingroom_text = self.check_in_waitingroom(
-                url, plz)
-            if in_waitingroom:
-                special = waitingroom_text
-                continue
-            print(trys, "Juhu")
-            try:
-
-                page_content = self.check_appointment_page(url, plz)
-                if page_content == "{}":
-                    print("EmptyPage")
-                    self.driver.delete_all_cookies()
-                    continue
-                output = json.loads(page_content)
-                print(plz, output)
-                break
-            except:
-                self.handle_page_error()
-                continue
-        return output, special
+        
+        in_waitingroom, waitingroom_text = self.check_in_waitingroom(
+            url, plz)
+        if in_waitingroom:
+            special = waitingroom_text
+            return output, special
+        try:
+            self.click_on_button()
+            page_content = self.check_appointment_page(url, plz)
+            if page_content == "{}":
+                print("EmptyPage")
+                self.driver.delete_all_cookies()
+                return output, special
+            output = json.loads(page_content)
+            print(plz, output)
+            return output, special
+        except:
+            self.handle_page_error()
+            return output, special
 
     def check_in_waitingroom(self, url, plz):
-        print("\tRefresh page")
-        self.driver.refresh()
-        time.sleep(15)
-        print("\tCall api")
+        print("Check if in waiting room")
         self.driver.get(url+"impftermine/service?plz="+str(plz))
+        try:
+            wait = WebDriverWait(self.driver, 10)
+            men_menu = wait.until(EC.visibility_of_element_located((By.XPATH, "(//body//div[@class='app-wrapper']/div[@class='cookies-info']//div[contains(@class,'row')]/div[contains(@class,'text-center')]/div[contains(@class,'row')]//a[contains(@class,'btn')])[1]")))
+            ActionChains(self.driver).move_to_element(men_menu).click().perform()
+        except:
+            print("No cookie button")
+        self.driver.save_screenshot("mainpage.png")
         special = None
         is_waitingroom = False
         if ("Virtueller Warteraum des Impfterminservice" in self.driver.page_source):
             print("warteraum")
             special = "warteraum"
             is_waitingroom = True
-            self.driver.save_screenshot("g.png")
+        else:
+            print("Done")
 
         return is_waitingroom, special
 
     def click_on_button(self):
-        for trys in range(5):
-            try:
-                radio_xpath = "//label[@class='ets-radio-control']"
-                btn = self.driver.find_elements_by_xpath(radio_xpath)[0]
-                ActionChains(self.driver).move_to_element(
-                    btn).click(btn).perform()
+        print("Click on button")
 
-                radio_xpath = "//label[@class='ets-radio-control']"
-                btn = self.driver.find_elements_by_xpath(radio_xpath)[1]
-                ActionChains(self.driver).move_to_element(
-                    btn).click(btn).perform()
-                break
-            except:
-                pass
+        wait = WebDriverWait(self.driver, 10)
+        men_menu = wait.until(EC.visibility_of_element_located((By.XPATH, "(//body//div[@class='app-wrapper']//div[contains(@class,'d-flex')]/div[@class='page-ets']/div[@class='container']//div[contains(@class,'row')]/div[contains(@class,'offset-1')]//div[contains(@class,'row')]//div//span)[2]")))
+        ActionChains(self.driver).move_to_element(men_menu).click().perform()
+        print("Done")
+        self.driver.save_screenshot("btn_click.png")
+
 
     def check_appointment_page(self, url, plz):
         termin_url = url
@@ -112,16 +157,36 @@ class Checker:
         termin_url += str(plz)
         termin_url += "&leistungsmerkmale=L920,L921,L922,L923"
         self.driver.get(termin_url)
-        div_xpath = "//div[@id='json']"
-        content_element = self.driver.find_element_by_xpath(div_xpath)
-        page_content = content_element.get_attribute("innerHTML")
+        div_xpath = "//pre"
+        
+        for i in range(5):
 
-        print("Rest Endpoint Loaded")
+            try:
+                print(self.driver.page_source)
+                print("Checking availablility",i,datetime.now())
+                self.driver.save_screenshot(str(i)+".png")
+                wait = WebDriverWait(self.driver, 20)
+                men_menu = wait.until(EC.visibility_of_element_located((By.XPATH, div_xpath)))
+                ActionChains(self.driver).move_to_element(men_menu).click().perform()
+                print("Done",datetime.now())
+                print(self.driver.page_source)
+                print("Trying to read availability from page")
+                content_element = self.driver.find_element_by_xpath(div_xpath)
+                page_content = content_element.get_attribute("innerHTML")
+                print(colored(page_content,"yellow"))
+                self.driver.save_screenshot("rest.png")
+                print("Done")
+                if "{}" in page_content:
+                    self.driver.refresh()
+                    continue
+                break
+            except:
+                self.driver.refresh()
 
         return page_content
 
     def handle_page_error(self):
-
+        
         self.driver.save_screenshot("fail.png")
         output = None
         if "wenden Sie sich bitte telefonisch" in self.driver.page_source:
@@ -131,7 +196,6 @@ class Checker:
             output = "noservice"
             print("No_Service")
         else:
-            traceback.print_exc()
-            print("Error after base page")
+            print("Error")
 
         return output
--- tor.py
+++ tor.py
@@ -1,25 +1,35 @@
 import subprocess
 import envir
 import time
+import os
 
 class Proxy_Handler:
     
     def __init__(self):
         self.proc=None
+        self.FNULL = open(os.devnull, 'w')
 
-    def start_proxy(self):
-        self.proc = subprocess.Popen(['torpy_socks', '-p', str(envir.proxy_port), '--hops', str(envir.proxy_hops)])
-        print()
-        print()
-        print()
-        print()
-        print("Sleeeeep")
-        time.sleep(30)
-        print("Sleeeeep")
+
+    def start_proxy(self,output=False):
+        # with output to console
+        print("Start tor connection")
+        if output:
+            self.proc = subprocess.Popen(['torpy_socks', '-p', str(envir.proxy_port), '--hops', str(envir.proxy_hops)])
+        else:
+            self.proc = subprocess.Popen(['torpy_socks', '-p', str(envir.proxy_port), '--hops', str(envir.proxy_hops)],stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        
+        for i in range(6):
+            print("sleeping",i,"/","6")
+            time.sleep(1)
+        print("Done connecting")
     
     def stop_proxy(self):
-        self.proc.terminate()
+        try:
+            self.proc.terminate()
+            return True
+        except:
+            return False
     
-    def restart_proxy(self):
-        self.proc.terminate()
-        self.proc = subprocess.Popen(['torpy_socks', '-p', str(envir.proxy_port), '--hops', str(envir.proxy_hops)])
+    def restart_proxy(self,output=False):
+        self.stop_proxy()
+        self.start_proxy(output)
